e07e6baea7112fcf612fef89289cf14164d49efc
Von <107226923+ArzuVon@users.noreply.github.com>
2022-08-07 20:49:03 -0400

    setting up index and auth routes for user

--- src/models/index.js
+++ src/models/index.js
@@ -12,25 +12,38 @@ const foodSchema = require('./food/model.js');
 const clothesSchema = require('./clothes/model.js');
 const recipeSchema = require('./recipe/model.js');
 const foodRecipeSchema = require('./foodRecipe/model.js');
+const userSchema = require('./user/model.js');
 
-const sequelizeOptions =
-  process.env.NODE_ENV === 'production'
+const baseOptions =
+  process.env.NODE_ENV === 'dev'
     ? {
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      }
-    : {};
+      logQueryParameters: true,
+      logging: (...args) => console.log(...args),
+    }
+    : {
+      logging: () => {},
+    };
 
+const sequelizeOptions = {
+  ...baseOptions,
+  ...(process.env.NODE_ENV === 'production'
+    ? {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+    : {}),
+};
 // turn schemas into Sequelize models
 const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
 const FoodModel = foodSchema(sequelize, DataTypes);
 const ClothesModel = clothesSchema(sequelize, DataTypes);
 const RecipeModel = recipeSchema(sequelize, DataTypes);
 const FoodRecipeModel = foodRecipeSchema(sequelize, DataTypes);
+const UserModel = userSchema(sequelize, DataTypes);
 
 // create our Collections and associations
 const FoodCollection = new Collection(FoodModel);
@@ -39,10 +52,13 @@ const RecipeCollection = new Collection(RecipeModel);
 FoodCollection.belongsToManyThrough(RecipeCollection, FoodRecipeModel);
 RecipeCollection.belongsToManyThrough(FoodCollection, FoodRecipeModel);
 
+const UserCollection = new Collection(UserModel);
+
 module.exports = {
   db: sequelize,
   Food: FoodCollection,
   Clothes: ClothesCollection,
   Recipe: RecipeCollection,
   FoodRecipe: FoodRecipeModel,
+  User: UserCollection,
 };
